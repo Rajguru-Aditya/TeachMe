e2f5e00b373249792a6087898b88b7364807f74a
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-06-30 12:28:04 +0530

    Add modal

---
+++ main.js
@@ -0,0 +1,25 @@
+const modal = document.querySelector(".myModal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".closeModal");
+const btnOpenModal = document.querySelector(".showModal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+btnOpenModal.addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
