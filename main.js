cbb1235bdc4f0995e6a9edf7bdc9743d27fe50f1
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-07-12 10:56:14 +0530

    Add Procfile and port for server

--- main.js
+++ main.js
@@ -48,10 +48,6 @@ app.post("/", function (req, res) {
       date: dateInput,
       slot: checkboxes.toString(),
     });
-    // console.log(nameInput);
-    // console.log(emailInput);
-    // console.log(dateInput);
-    // console.log(checkboxes.toString());
     student.save();
     res.render("success");
   }
@@ -61,6 +57,11 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully");
 });
